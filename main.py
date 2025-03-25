afa084b9cbcd9a643f3bf7d50ed2266977b0ea9d
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2025-03-25 13:31:15 -0300

    Refactor prompt handling in main.py to utilize PromptTemplate for itinerary generation, enhancing readability and maintainability.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
 
 numero_de_dias = 7
 numero_de_criancas = 2
 atividade = "praia"
 
-prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
+modelo_do_prompt = PromptTemplate.from_template( 
+  "Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
+
+)
+
+prompt = modelo_do_prompt.format(numero_de_dias=numero_de_dias, numero_de_criancas=numero_de_criancas, atividade=atividade)
+
 print(prompt)
 
 llm = ChatOpenAI(model="gpt-3.5-turbo", api_key="", temperature=0.5)
