02de822d2faf8cce6a23f24e85938ab192a7cc47
Tarcisio Philips <tarcisiogabriel.philips@gmail.com>
2025-03-25 09:40:10 -0300

    Refactor main.py to use langchain_openai's ChatOpenAI for generating travel itineraries, replacing OpenAI API calls. Update prompt handling and response extraction accordingly.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from openai import OpenAI
+from langchain_openai import ChatOpenAI
 
 numero_de_dias = 7
 numero_de_criancas = 2
@@ -7,17 +7,11 @@ atividade = "praia"
 prompt = f"Crie um roteiro de viagem de {numero_de_dias} dias, para uma família com {numero_de_criancas} crianças, que gostam de {atividade}."
 print(prompt)
 
-cliente = OpenAI(api_key="")
+llm = ChatOpenAI(model="gpt-3.5-turbo", api_key="", temperature=0.5)
 
-resposta = cliente.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt}
-    ]
-)
+resposta = llm.invoke(prompt)
 
 print(resposta)
 
 roteiro_viagem = resposta.choices[0].message.content
-print(roteiro_viagem)
+print(roteiro_viagem.content)
